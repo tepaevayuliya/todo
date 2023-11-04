bce26ac78c9d7fa666b212e31a8712425a1c7d37
Yuliya Tepaeva <tepaevayuliya@yandex.ru>
2023-11-04 14:53:17 +0300

    lesson 3 (02.11.2023)

---
+++ todo/Classes/Modules/Parent/ParenyViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ParenyViewController.swift
+//  todo
+//
+//  Created by Юлия Тепаева on 02.11.2023.
+//
+
+import Foundation
---
+++ todo/UIKit/TextField.swift
@@ -0,0 +1,8 @@
+//
+//  TextField.swift
+//  todo
+//
+//  Created by Юлия Тепаева on 02.11.2023.
+//
+
+import Foundation
