7a037ab508abd53f3fbf72929c1ab0d99a001fa6
Yuliya Tepaeva <tepaevayuliya@yandex.ru>
2023-11-12 18:07:03 +0300

    lesson 4 (09.11.2023)

---
+++ todo/UIKit/Button/PrimaryButton.swift
@@ -0,0 +1,23 @@
+//
+//  PrimaryButton.swift
+//  todo
+//
+//  Created by Юлия Тепаева on 09.11.2023.
+//
+
+import UIKit
+
+final class PrimaryButton: MainButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    private func setup() {
+        style = Style(cornerRadius: 8, insets: 32, bgColor: .Color.primary, highlightedBgColor: .Color.primary.withAlphaComponent(0.5), titleColor: .Color.white)
+    }
+}
